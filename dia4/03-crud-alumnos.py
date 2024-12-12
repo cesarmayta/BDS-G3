0fe95d5a40155074e6e8ae5d2b3df9abe2d03966
César Mayta Avalos <cesarmayta@gmail.com>
2024-12-11 21:22:53 -0500

    crud funciones

--- dia4/03-crud-alumnos.py
+++ dia4/03-crud-alumnos.py
@@ -1,7 +1,6 @@
-from matriculas.lib_alumnos import *
-
 import os
 from time import sleep
+from matriculas.lib_alumnos import *
 
 """
 CRUD
@@ -22,43 +21,15 @@ opcion = 0
 
 while(opcion < 5):
     os.system("clear")
-    print("="*ANCHO)
-    print(" " * 10 + "GESTIÓN DE ALUMNOS")
-    print("="*ANCHO)
-    print("""
-         [1] REGISTRAR ALUMNO
-         [2] MOSTRAR ALUMNOS
-         [3] ACTUALIZAR ALUMNO
-         [4] ELIMINAR ALUMNO
-         [5] SALIR
-          """)
-    print("=" * ANCHO)
+    menu(ANCHO)
     opcion = int(input("INGRESE OPCION : "))
     os.system("clear")
 
     if opcion == 1:
-        print("=" * ANCHO)
-        print(" " * 10 + "[1] REGISTRAR ALUMNO")
-        print("=" * ANCHO)
-        dni = input("DNI    :")
-        nombre = input("NOMBRE  :")
-        email = input("EMAIL    :")
-        dic_nuevo_alumno = {
-            dni : {
-                   'nombre':nombre,
-                   'email': email
-                  }
-        }
+        dic_nuevo_alumno = registrar(ANCHO) 
         dic_alumnos.update(dic_nuevo_alumno)
     elif opcion == 2:
-        print("=" * ANCHO)
-        print(" " * 10 + "[2] MOSTRAR ALUMNOS")
-        print("=" * ANCHO)
-        for dni,datos in dic_alumnos.items():
-            print(f"DNI : {dni}")
-            print(f"Nombre : {datos['nombre']}")
-            print(f"EMAIL : {datos['email']}")
-            print("*"*ANCHO)
+        mostrar(ANCHO,dic_alumnos)
         input("Presion ENTER para continuar...")
     elif opcion == 3:
         print("=" * ANCHO)
@@ -70,10 +41,24 @@ while(opcion < 5):
             nuevo_nombre = input('NOMBRE : ')
             nuevo_dni = input('DNI  :')
             nuevo_email = input('EMAIL')
+            dic_act_alumno = {
+                dni : {
+                    'nombre':nuevo_nombre,
+                    'email':nuevo_email
+                }
+            }
+            dic_alumnos.update(dic_act_alumno)
+            print("ALUMNO ACTUALIZADO CON EXITO")
     elif opcion == 4:
         print("=" * ANCHO)
         print(" " * 10 + "[4] ELIMINAR ALUMNO")
         print("=" * ANCHO)
+        dni = input("INGRES EL DNI DEL ALUMNO A ELIMINAR : ")
+        if dni in dic_alumnos:
+            dic_alumnos.pop(dni)
+            print("ALUMNO ELIMINADO")
+        else:
+            print("NO SE ENCONTRO EL ALUMNO")
     elif opcion == 5:
         print("=" * ANCHO)
         print(" " * 10 + "[5] SALIR")
