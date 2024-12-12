4cf3645fb259fa2b3f4116ec34375c1064f6aa89
César Mayta Avalos <cesarmayta@gmail.com>
2024-12-11 23:00:21 -0500

    crud con archivo

--- dia4/03-crud-alumnos.py
+++ dia4/03-crud-alumnos.py
@@ -9,6 +9,7 @@ CRUD
  - UPDATE
  - DELETE
 """
+cargar_alumnos('alumnos.txt')
 opcion = 0
 
 while(opcion < 5):
@@ -26,6 +27,7 @@ while(opcion < 5):
     elif opcion == 4:
         eliminar()
     elif opcion == 5:
+        grabar_alumnos('alumnos.txt')
         mostrar_mensaje("[5] SALIR")
     else:
         mostrar_mensaje("OPCIÓN INVÁLIDA!!!")
