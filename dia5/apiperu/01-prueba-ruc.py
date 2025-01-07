b5a7bd24eb9475e240d63a32415d3120b904eb3a
César Mayta Avalos <cesarmayta@gmail.com>
2025-01-06 22:33:11 -0500

    apiperu

---
+++ dia5/apiperu/01-prueba-ruc.py
@@ -0,0 +1,22 @@
+import requests
+
+TOKEN = ''
+API_URL = 'https://apiperu.dev/api/ruc'
+
+ruc = input('Ingrese el RUC: ')
+
+data_request = {
+    "ruc": ruc
+}
+
+headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(API_URL, json=data_request, headers=headers)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Error: {response.status_code} - {response.text}")
