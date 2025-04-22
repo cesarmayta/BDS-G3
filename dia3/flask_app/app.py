762738e2e14856e072ebc0ce02c81ed529e9e527
César Mayta Avalos <cesarmayta@gmail.com>
2025-04-21 20:25:31 -0500

    resultado ejercicio

--- dia3/flask_app/app.py
+++ dia3/flask_app/app.py
@@ -29,4 +29,22 @@ def resta(num1, num2):
 # crear una ruta en donde yo coloque la operacion/n1/n2 ejem suma/3/4 resta/4/2 multiplicacion/2/1 division/4/2
 # la función debe llamarse operaciones
 
+@app.route('/<operacion>/<int:num1>/<int:num2>')
+def operaciones(operacion, num1, num2):
+    if operacion == 'suma':
+        resultado = num1 + num2
+    elif operacion == 'resta':
+        resultado = num1 - num2
+    elif operacion == 'multiplicacion':
+        resultado = num1 * num2
+    elif operacion == 'division':
+        if num2 != 0:
+            resultado = num1 / num2
+        else:
+            return "<h1><center>Error: División por cero no permitida.</center></h1>"
+    else:
+        return "<h1><center>Error: Operación no válida.</center></h1>"
+    
+    return f"<h1><center>El resultado de {operacion} entre {num1} y {num2} es {resultado}</center></h1>"
+
 app.run(debug=True)
