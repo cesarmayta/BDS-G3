80c6afc73205e4d9356b5bdd61a3add9d2b1013f
César Mayta Avalos <cesarmayta@gmail.com>
2025-04-21 19:59:34 -0500

    request

--- dia3/flask_app/app.py
+++ dia3/flask_app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1><center>Hola mundo con Flask!</center></h1>"
+
+@app.route('/saludo')
+def saludo():
+    nombre = request.args.get('nombre', 'Mundo')
+    return f"<h1><center>Hola {nombre}!</center></h1>"
+
 
 app.run(debug=True)
