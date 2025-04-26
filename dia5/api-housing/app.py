f6b77a04d6f7c676bf95274296cf69e52100a438
César Mayta Avalos <cesarmayta@gmail.com>
2025-04-25 22:27:14 -0500

    housing frontend

--- dia5/api-housing/app.py
+++ dia5/api-housing/app.py
@@ -2,9 +2,11 @@ from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from model import HousingModel
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 app.app_context().push()
 ### CREAMOS NUESTRA PRIMER TABLA USANDO EL ORM DE SQLALCHEMY ###
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/db_mlg3'
