183900f0822c5f4180fad93442f59a23e3e80918
César Mayta Avalos <cesarmayta@gmail.com>
2025-04-25 20:12:36 -0500

    sqlalchemy

--- dia5/api-housing/app.py
+++ dia5/api-housing/app.py
@@ -1,7 +1,26 @@
 from flask import Flask,request,jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.app_context().push()
+### CREAMOS NUESTRA PRIMER TABLA USANDO EL ORM DE SQLALCHEMY ###
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/db_mlg3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+class Housing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rooms = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Double,nullable=True)
+    
+    def __init__(self, rooms):
+        self.rooms = rooms
+
+db.create_all()
+print("Base de datos creada")
+
+##################################################################
 
 @app.route('/')
 def index():
