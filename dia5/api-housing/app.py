99f216ccefae688ad3b832c9abfcc430e797cd96
César Mayta Avalos <cesarmayta@gmail.com>
2025-04-25 20:26:20 -0500

    housing post

--- dia5/api-housing/app.py
+++ dia5/api-housing/app.py
@@ -31,4 +31,20 @@ def index():
     
     return jsonify(context)
 
+@app.route('/housing', methods=['POST'])
+def set_data():
+    rooms = request.json['rooms']
+    new_housing = Housing(rooms)
+    
+    #insertamos el nuevo registro en la base de datos
+    db.session.add(new_housing)
+    db.session.commit()
+    
+    context = {
+        'status':True,
+        'message': 'Registro creado',
+    }
+    
+    return jsonify(context, 201)
+
 app.run(debug=True)
